90fd247cc7fb88f960064f33045e5bec07d0c26e
Nikhil <nikhilmht33@gmail.com>
2020-09-27 12:40:14 +0530

    Add StringExtensions and ParseExtensions

--- library/src/main/java/com/nikhil/library/ParseExtensions.kt
+++ library/src/main/java/com/nikhil/library/ParseExtensions.kt
@@ -1,5 +1,8 @@
 package com.nikhil.library
 
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.*
 import kotlin.experimental.and
 
@@ -52,4 +55,122 @@ private fun decodeBytes(str: String): ByteArray {
         i += 2
     }
     return bytes
+}
+
+/**
+ * JSON and XML are two popular formats for data load, save, exchange, etc. It’s hard to imagine API call without one of these formats.
+ * Java offers rather comfortable classes: [JSONObject] and [JSONArray].
+ * Kotlin can use any Java class, so it’s a Kotlin way to parse and write JSON too. [JSONObject] and [JSONArray] are available in Android.
+ *
+ * Why extend these classes?
+ * The problem is that they throw exception if something goes wrong.
+ * If anything goes wrong.
+ * For example, if you try to get the wrong data type, or if you refer to a field that doesn’t exist.
+ * <code>
+ *     try {
+val obj = JSONObject()
+obj.put("str", "abc")
+val strAsInt = obj.getInt("str") // org.json.JSONException: Value abc at str of type java.lang.String cannot be converted to int
+val noKey = obj.getString("iamnothere") // org.json.JSONException: No value for iamnothere
+}
+catch (e: JSONException) {
+e.printStackTrace()
+}
+<code>
+ *
+ * There’s nothing wrong with exceptions; it’s a good way to handle such situations.
+ * But sometimes we need to pull only existing fields and ignore (keep as null) fields, which are not in API response.
+ * Without extensions it can be done this way:
+ * <code>
+ *     val firstName: String?
+ *     try {
+ *          firstName = json.getString("first_name")
+ *      } catch (e: JSONException) {
+ *          firstName = null
+ *      }
+ *
+ *      val lastName: String?
+ *      try {
+ *          lastName = json.getString("last_name")
+ *      } catch (e: JSONException) {
+ *          lastName = null
+ *      }
+ *
+ * Looks very long, right?
+ * Now imagine that server returns "null" instead of null. Code will grow for each field.
+ * This can be solved with extension:
+ *
+ * </code>
+ */
+fun JSONObject.getIntOrNull(name: String): Int? =
+    try {
+        getInt(name)
+    } catch (e: JSONException) {
+        val strValue = getStringOrNull(name)
+        strValue?.toIntOrNull()
+    }
+
+fun JSONObject.getDoubleOrNull(name: String): Double? =
+    try {
+        getDouble(name)
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getLongOrNull(name: String): Long? =
+    try {
+        getLong(name)
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getStringOrNull(name: String): String? =
+    try {
+        getString(name).trim()
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getBooleanOrNull(name: String): Boolean? =
+    try {
+        getBoolean(name)
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getObjectOrNull(name: String): JSONObject? =
+    try {
+        getJSONObject(name)
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getArrayOrNull(name: String): JSONArray? =
+    try {
+        getJSONArray(name)
+    } catch (e: JSONException) {
+        null
+    }
+
+fun JSONObject.getArrayOrEmpty(name: String): JSONArray =
+    try {
+        getJSONArray(name)
+    } catch (e: JSONException) {
+        JSONArray()
+    }
+
+/**
+ * This extension has some additional features like String trimming.
+ * Features like replacing "null" to null can be added this way:
+ */
+fun JSONObject.getStringOrNull2(name: String): String? {
+    try {
+        val str = getString(name).trim()
+        if (str == "null")
+            return null
+        return str
+    } catch (e: JSONException) {
+        null
+    }
+    return null
 }
---
+++ library/src/main/java/com/nikhil/library/StringExtensions.kt
@@ -0,0 +1,249 @@
+package com.nikhil.library
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.security.MessageDigest
+import java.util.regex.Pattern
+
+/**
+ * MD5 Hash Calculator
+ * Reasons to calculate MD5 of a [String]:
+ * Save password in database, make some verification through insecure channel, check if file was calculated correctly.
+ * Same with SHA hashes.
+ */
+val String.md5: String
+    get() {
+        //get MD5 hash as byte array
+        val bytes = MessageDigest.getInstance("MD5").digest(this.toByteArray())
+        //convert byte array to hex string
+        return bytes.joinToString("") {
+            "%02x".format(it)
+        }
+    }
+
+/**
+ * SHA1 Hash Calculator
+ */
+val String.sha1: String
+    get() {
+        val bytes = MessageDigest.getInstance("SHA-1").digest(this.toByteArray())
+        return bytes.joinToString("") {
+            "%02x".format(it)
+        }
+    }
+
+/**
+ * Checking If a String Is a Valid Email Address
+ */
+fun String.isEmailValid(): Boolean {
+    val expression = "^[\\w.-]+@([\\w\\-]+\\.)+[A-Z]{2,8}$"
+    val pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE)
+    val matcher = pattern.matcher(this)
+    return matcher.matches()
+}
+
+/**
+ * Phone numbers for different countries have different formats. That’s why it can be done only with an external library.
+ * This function uses (https://github.com/MichaelRocks/libphonenumber-android) library
+ *
+ * This function returns formatted phone number (starting with “+” and country code) if it’s valid, or null if not.
+ * First argument is Android [Context], for example, [Activity].
+ */
+fun String.formatPhoneNumber(context: Context, region: String): String? {
+    val phoneNumberKit = PhoneNumberUtil.createInstance(context)
+    val number = phoneNumberKit.parse(this, region)
+    if (!phoneNumberKit.isValidNumber(number))
+        return null
+    return phoneNumberKit.format(number, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
+}
+
+/**
+ * What is inside the [String]?
+ * It can be a number, it can be a word, or it can be a password with special characters.
+ * Sometimes we need to find out what’s inside the string to know what to do next.
+ */
+val String.containsLatinLetter: Boolean
+    get() = matches(Regex(".*[A-Za-z].*"))
+
+val String.containsDigit: Boolean
+    get() = matches(Regex(".*[0-9].*"))
+
+val String.isAlphanumeric: Boolean
+    get() = matches(Regex("[A-Za-z0-9]*"))
+
+val String.hasLettersAndDigits: Boolean
+    get() = containsLatinLetter && containsDigit
+
+val String.isIntegerNumber: Boolean
+    get() = toIntOrNull() != null
+
+val String.toDecimalNumber: Boolean
+    get() = toDoubleOrNull() != null
+
+/**
+ * Storing and Retrieving Local Settings
+ * Android apps have several ways to store local settings, but the most simple and native way is [SharedPreferences].
+ *
+ * First argument is applicationContext: [Context]. If you run function from [Activity], it can pass itself (as Activity is subclass of Context) or better, applicationContext.
+ *
+ * Second argument is value: Map<String, Any>. [Map]s in Kotlin are easy to create and use.
+ * In this extension Any can be any non-null value, but only five types will be saved: [Int], [Float], [Long], [Boolean] and [String].
+ *
+ * Third argument is clear
+ * @param clear should be true if all fields in [SharedPreferences], which are not in
+ * @param value, should be removed from SharedPreferences.
+ *
+ * Fourth argument is now
+ * Another detail about [SharedPreferences] is background saving.
+ * There are two ways to save SharedPreferences: [commit] and [apply].
+ * The difference is that [commit] applies changes immediately. [apply] does it in the background, which can make your app faster, but doesn’t guarantee immediate saving.
+ * In most cases [apply] is preferred, that’s why 'now' argument should be false almost always.
+ *
+ * Please note that here [SharedPreferences] are saved with [apply].
+ * At the same time, it can be used immediately.
+ * It happens because [SharedPreferences] are stored in phone memory.
+ * If the app crashes right after, there’s a small chance that value won’t be saved, especially if it’s big.
+ */
+@SuppressLint("ApplySharedPref")
+fun String.save(
+    applicationContext: Context,
+    value: Map<String, Any>,
+    clear: Boolean = false,
+    now: Boolean = false
+) {
+    val sp = applicationContext.getSharedPreferences(this, Context.MODE_PRIVATE).edit()
+    if (clear)
+        sp.clear()
+    value.keys.forEach { key ->
+        val v = value[key]
+        if (v != null) {
+            when (v) {
+                is String -> sp.putString(key, v)
+                is Float -> sp.putFloat(key, v)
+                is Long -> sp.putLong(key, v)
+                is Int -> sp.putInt(key, v)
+                is Boolean -> sp.putBoolean(key, v)
+            }
+        }
+    }
+    if (now)
+        sp.commit()
+    else
+        sp.apply()
+}
+
+fun String.load(applicationContext: Context): Map<String, Any> {
+    val sp = applicationContext.getSharedPreferences(this, Context.MODE_PRIVATE)
+    val keys = sp.all.keys
+    val result = hashMapOf<String, Any>()
+    keys.map { key ->
+        val v = sp.all[key]
+        if (v != null)
+            result[key] = v
+    }
+    return result
+}
+
+/**
+ * If [String] contains a valid JSON, function will return [JSONObject], otherwise null.
+ */
+val String.jsonObject: JSONObject?
+    get() = try {
+        JSONObject(this)
+    } catch (e: JSONException) {
+        null
+    }
+
+/**
+ * If [String] contains a valid JSON, function will return [JSONArray], otherwise null.
+ */
+val String.jsonArray: JSONArray?
+    get() = try {
+        JSONArray(this)
+    } catch (e: JSONException) {
+        null
+    }
+
+/**
+ * If [String] contains a path or URL, it returns last component (after last ‘/’).
+ *
+ * Two important points:
+ * 1. Path can end with trailing slash. The extension function should return path component before it; otherwise, it won’t process URLs.
+ * 2. Some systems (Microsoft Windows, for example) use backslash instead of slash. We should handle it too.
+ */
+val String.lastPathComponent: String
+    get() {
+        var path = this
+        if (path.endsWith("/"))
+            path = path.substring(0, path.length - 1)
+        var index = path.lastIndexOf('/')
+        if (index < 0) {
+            if (path.endsWith("\\"))
+                path = path.substring(0, path.length - 1)
+            index = path.lastIndexOf('\\')
+            if (index < 0)
+                return path
+        }
+        return path.substring(index + 1)
+    }
+
+/**
+ * Color From a String
+ *
+ * Color is represented differently in different environments.
+ * Android uses simple integers. It has [Color] class, but it just contains helper functions.
+ * Another representation is AWT Color class, which incapsulates four integer values for red, green, blue and alpha components.
+ */
+
+/**
+ * Parsing hex String to AWT Color
+ */
+//val String.awtColor: Color?
+//    get() {
+//        val r = substring(1, 3).toIntOrNull(16) ?: return null
+//        val g = substring(3, 5).toIntOrNull(16) ?: return null
+//        val b = substring(5, 7).toIntOrNull(16) ?: return null
+//        return Color(r, g, b)
+//    }
+
+/**
+ * Parsing hex String to Android Color
+ *
+ * This class returns [Int] because Android uses [Int] in its views.
+ *
+ * You may ask: Why use extension if it replaces one line of code with another line of code?
+ * There are two differences:
+ * 1. 'asColor' extension can be chained. For example: "hexString.asColor?.toString()"
+ * 2. 'null' instead of exception. You don’t need to wrap it in try-catch block. You’ll just get 'null' if it’s not a valid color.
+ */
+val String.asColor: Int?
+    get() = try {
+        Color.parseColor(this)
+    } catch (e: java.lang.IllegalArgumentException) {
+        null
+    }
+
+/**
+ * Format String as Credit Card Number
+ *
+ * This simple extension adds space after each four characters of a string
+ */
+val String.creditCardFormatted: String
+    get() {
+        val preparedString = replace(" ", "").trim()
+        val result = StringBuilder()
+        for (i in preparedString.indices) {
+            if (i % 4 == 0 && i != 0) {
+                result.append(" ")
+            }
+            result.append(preparedString[i])
+        }
+        return result.toString()
+    }
