baae3ccb59e3415243375cbfb65336592bb1aa12
Nikhil <nikhilmht33@gmail.com>
2021-02-04 20:19:02 +0530

    Add viewModelScope.launch example

--- library/src/main/java/com/nikhil/library/android/ExampleViewModel.kt
+++ library/src/main/java/com/nikhil/library/android/ExampleViewModel.kt
@@ -2,10 +2,19 @@ package com.nikhil.library.android
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class ExampleViewModel : ViewModel() {
 
     private val _showProgress = MutableStateFlow(false)
     val showProgress = _showProgress.asLiveData()
+
+    init {
+        // This Coroutine will be executed on Dispatchers.Main so it’s better to specify another dispatcher (such as Dispatcher.IO) if you have some heavy work
+        viewModelScope.launch {
+
+        }
+    }
 }
