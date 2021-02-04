a31cc91f1c102cf26c1cc1496afb8ca4a509c22b
Nikhil <nikhilmht33@gmail.com>
2021-02-04 20:18:21 +0530

    Add CoroutineScope launch and whenResumed example

--- library/src/main/java/com/nikhil/library/android/FragmentExxtensions.kt
+++ library/src/main/java/com/nikhil/library/android/FragmentExxtensions.kt
@@ -1,9 +1,14 @@
 package com.nikhil.library.android
 
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.commit
+import androidx.lifecycle.whenResumed
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class FragmentExxtensions : Fragment() {
 
@@ -16,6 +21,21 @@ class FragmentExxtensions : Fragment() {
         // extensions like add, replace, commit can simplify our transactions with lambdas
         parentFragmentManager.commit {
             addToBackStack("")
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            whenResumed {
+                // do some stuff
+            }
+        }
+    }
+
+    suspend fun ex() {
+        viewLifecycleOwner.lifecycle.whenResumed {
         }
     }
 }
